10454aa63e68241f767189cbc8148499af804975
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-07-19 01:44:06 +0500

    slider img finished

---
+++ JS hometasks/24-lesson-hometask-JS/js/slider.js
@@ -0,0 +1,26 @@
+const image = document.getElementById('img1');
+const text = document.getElementById('h1')
+const images = [
+    {
+        img:`/JS hometasks/24-lesson-hometask-JS/images/slider-im-1.jpg`,
+        txt: 'Picture 1',
+    },
+    {
+        img:`/JS hometasks/24-lesson-hometask-JS/images/slider-im-2.webp`,
+        txt: 'Picture 2',
+    },
+    {
+        img:`/JS hometasks/24-lesson-hometask-JS/images/slider-im-3.webp`,
+        txt: 'Picture 3',
+    },
+]
+let currentImg = 0;
+
+function sliderImg() {
+    currentImg++;
+    if (currentImg > images.length-1) {
+        currentImg = 0
+    }
+    image.src = images[currentImg].img;
+    text.textContent = images[currentImg].txt
+}
