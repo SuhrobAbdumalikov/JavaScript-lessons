8111dffe968540e0e530a5093c3eec33550830ba
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-07-19 15:47:59 +0500

    menu bars finished

--- JS hometasks/24-lesson-hometask-JS/Gamburger Menu/menu.js
+++ JS hometasks/24-lesson-hometask-JS/Gamburger Menu/menu.js
@@ -1,13 +1,16 @@
-const Menu = document.getElementsByTagName('aside');
-
-let OpenInfo = false
+const Menu = document.getElementsByTagName("aside");
+const bars = document.getElementsByTagName("i");
+let OpenInfo = false;
 function MenuInfo() {
-    if (OpenInfo) {
-        Menu[0].removeAttribute('class');
-        OpenInfo = false;
-    }else{
-        Menu[0].setAttribute('class','active');
-        OpenInfo = true;
-    }
+  if (OpenInfo) {
+    Menu[0].removeAttribute("class");
+    bars[0].setAttribute("class", "fa-solid fa-bars fa-xl");
+    OpenInfo = false;
+  } else {
+    Menu[0].setAttribute("class", "active");
+    bars[0].setAttribute("class", " fa-solid fa-xmark fa-xl");
+    OpenInfo = true;
+    // bars[0].setAttribute("fa-solid fa-xmark",'class');
+  }
 }
 // const btn = document.getElementById('btn').addEventListener('click',MenuInfo)
