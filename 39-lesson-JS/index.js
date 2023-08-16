a860ff2c574ce0799977ed0d07f7464f95007d05
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-08-17 00:22:43 +0500

    39-lesson

---
+++ 39-lesson-JS/index.js
@@ -0,0 +1,10 @@
+const arr = [2, 3, 4, 1, 5]; //result => [[3,2,4,1],[2,4,5,1]];
+let r = {};
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    r[arr[i] + arr[j]] = r[arr[i] + arr[j]]
+      ? r[arr[i] + arr[i + 1]]
+      : r[arr[i] + arr[i + 1]];
+  }
+}
+console.log(r);
